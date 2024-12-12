673dbaad3893d19fe0ff2b8259c7d15f6be14e8d
Zaker Safdari <zakersafdari@gmail.com>
2024-12-12 17:31:01 +1300

    renamed project

--- vue-frontend/src/components/ChatApp.vue
+++ vue-frontend/src/components/ChatApp.vue
@@ -24,7 +24,7 @@
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "vue";
 import { startChat, sendMessage } from "../services/chatService";
-import MarkdownIt from "markdown-it";
+import MarkdownIt from "markdown-it/index.js";
 
 export default defineComponent({
   name: "ChatApp",
