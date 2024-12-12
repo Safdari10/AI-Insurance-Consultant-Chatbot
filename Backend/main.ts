08cd4dac9e65fc344757a3c8d49adf4b6abeae30
Zaker Safdari <zakersafdari@gmail.com>
2024-12-12 15:37:17 +1300

    added cors

--- Backend/main.ts
+++ Backend/main.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono@latest';
+import { cors } from 'hono@latest/cors';
 import chatRoutes from './routes/chatRoutes.ts';
 
 const app = new Hono();
 
+app.use('*', cors());
+
 app.route('/chat', chatRoutes);
 
-Deno.serve({ port: 3000 }, app.fetch)
+Deno.serve({ port: 3000 }, app.fetch);
